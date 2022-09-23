7892257307622fe16ccd3ac326aa943f7b093299
Dwipayan Barman <dbarman8697@gmail.com>
2022-09-23 13:02:28 +0530

    Prime Problem Solve by DWIP

--- prime.js
+++ prime.js
@@ -1,10 +1,20 @@
-function prime(num){
-   for(let i=1; i<=num; i++){
-       if(num%i){
-           return true;
+function prime(number){
+   let factors = 0;
+   for(let i=1; i<=number; i++){
+       if(number % i==0){
+           factors++;
         }
     }
-    return false;
+    if (factors==2){
+    return true; 
+  }
+   return false;
 }
 
-prime(13);
+   let ans = prime(13);
+   if(ans ==true){
+  
+   console.log("Prime");
+  }else{
+   console.log("Not Prime");
+  }
